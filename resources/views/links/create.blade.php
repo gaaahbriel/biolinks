b6502da29b5ad0d55c22d0224ba4fd5a17ac7f28
bonfim <gabriel.bonfim111@gmail.com>
2026-06-01 13:28:24 -0300

    Feat: Criação dos controllers, migration, request, model, polities, factory, seeders e view da função de criação de links

---
+++ resources/views/links/create.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <h1>Criar um link</h1>
+
+    @if($message = session()->get('message'))
+    <p>{{ $message }}</p>
+    @endif
+
+    <form action="{{ route('links.create') }}" method="POST">
+        @csrf
+        <div>
+            <input type="text" name="link" placeholder="Link" value="{{ old('link') }}">
+            @error('link')
+            <p>{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <input type="text" name="name" placeholder="Name" value="{{ old('name') }}">
+            @error('name')
+            <p>{{ $message }}</p>
+            @enderror
+        </div>
+        <div><button type="submit">Criar um link</button></div>
+    </form>
+</div>
